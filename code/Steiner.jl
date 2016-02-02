222ef2a54276e1565365228361c58e8bfa04aff6
eoudet <edouard.oudet@imag.fr>
2016-02-02 17:16:17 +0100

    with Standalone

--- code/Steiner.jl
+++ code/Steiner.jl
@@ -1,8 +1,9 @@
 module Steiner
 
-using Base.LinAlg, Gadfly, NearestNeighbors, MAT
+using Base.LinAlg, NearestNeighbors, MAT
 using CPLEX, MathProgBase, Gurobi
 using GeneralTools, GraphicTools, MeshTools, ColorsTools
+include(string(ENV["HOME"],"/Julia/Steiner/code/Steinersetup.jl"))
 
 export steinerLP, steinerCPLEX
 
@@ -159,77 +160,6 @@ end
 
 
 #----------------------------------------------------------
-function gensteiner_mesh(steinertype::Int64=1,targetp::Array{Float64,2}=zeros(1,3),np::Int64=400;
-                         dim::Int64=2,nearestk::Int64=30)
-    
-    if steinertype==1 # 1 <-> delaunay of random points in [-1,1]
-        p = vcat(2.*rand(np-size(targetp,1),dim)-1,targetp[:,1:dim])
-        tri = delaunay(p)
-        ep = convert(Array{Int64,2},vcat(tri[:,[1,2]],tri[:,[2,3]],tri[:,[1,3]]))
-    elseif steinertype==2 # 2 <-> Nearest neighbor type  [-1,1]
-        p = vcat(2*rand(np-size(targetp,1),dim)-1,targetp[:,1:dim])
-        tree = KDTree(p[:,1:dim]',Euclidean();leafsize=2)
-        println(tree)
-        dists = zeros(size(p,1),nearestk)
-        idxs = zeros(Int64,size(p,1),nearestk)
-        ep = zeros(Int64,size(p,1)*nearestk,2)
-        for k = 1:size(p,1)
-            idxst, distst = knn(tree,p[k,1:dim],nearestk)
-            ep[((k-1)*nearestk+1):k*nearestk,1] = k
-            ep[((k-1)*nearestk+1):k*nearestk,2] = idxst
-            #println(idxst,"\n",distst)
-        end
-    elseif steinertype==3 # 3 <-> Nearest neighbor on a grid of [-1,1]
-        npg = Int64(max(ceil(np^(1./dim)),5)); xx = linspace(-1,1,npg)
-        if dim==2
-            X, Y = Main.ndgrid(xx,xx)
-            Z =zeros(size(X))
-        else
-            X, Y, Z = Main.ndgrid(xx,xx,xx)
-        end
-        pts = hcat(X[:],Y[:],Z[:]); pts = pts + (2.*rand(size(pts))-1.)/10000.
-        p = vcat(pts[:,1:dim],targetp[:,1:dim])
-        tree = KDTree(p[:,1:dim]',Euclidean();leafsize=2)
-        println(tree)
-        dists = zeros(size(p,1),nearestk)
-        idxs = zeros(Int64,size(p,1),nearestk)
-        ep = zeros(Int64,size(p,1)*nearestk,2)
-        for k = 1:size(p,1)
-            idxst, distst = knn(tree,p[k,1:dim],nearestk)
-            ep[((k-1)*nearestk+1):k*nearestk,1] = k
-            ep[((k-1)*nearestk+1):k*nearestk,2] = idxst
-            #println(idxst,"\n",distst)
-        end
-    end
-    ep = uniquerows(sort(ep,2))[1]
-    em = hcat(ep[:,2],ep[:,1]); e = vcat(ep,em); 
-    np = size(p,1);  ne = size(e,1)
-    return p, e, np, ne
-end
-
-#----------------------------------------------------------
-function define_targets(dim::Int64=2,npt::Int64=4)
-    # Target points
-    if dim==2
-        tt = linspace(0.,2*pi,npt+1)[1:(end-1)]
-        targetp = hcat(cos(tt),sin(tt),zeros(npt))*.95
-        if npt>8
-            targetp = 2*rand(npt,2) - 1.
-        end
-    elseif dim==3
-        ST, FT, SH, FH, SO, FO, SD, FD, SI, FI = polyedres()
-        if npt==4
-            targetp = ST           
-        elseif npt==8
-            targetp = SH           
-        end   
-    end
-    targetp = targetp - repmat(mean(targetp,1),size(targetp,1),1)
-    targetp = 0.9*targetp/maximum(abs(targetp))
-    return targetp, size(targetp,1)
-end
-
-#----------------------------------------------------------
 function displaysave_steinertree(t::Array{Float64,1},s::Array{Float64,1},e::Array{Int64,2},
                                  p::Array{Float64,2},targetp::Array{Float64,2},fignum::Int64,savedata::Bool)
     dim = size(p,2); np = size(p,1); npt = size(targetp,1); ve = 1 - max(0,t-s)
@@ -240,7 +170,7 @@ function displaysave_steinertree(t::Array{Float64,1},s::Array{Float64,1},e::Arra
     ve = min(ve[1:esm],ve[(esm+1):end]); Ie = find(x->x<0.1,ve)
     minmaxmean(ve)
     println(InOrange*string(unique(round(ve*1e3)/1000))*InDefault)
-    vec2 = plot_graph(p,ep[Ie,:],ve[Ie,1],figure=fignum,line_width=5.*(dim-1.),vmin=0.,vmax=1.);setcolormap(vec2,"Oranges")
+    vec2 = plot_graph(p,ep[Ie,:],ve[Ie,1],figure=fignum,line_width=(5+10*(dim-2.)),vmin=0.,vmax=1.,opacity=0.3);setcolormap(vec2,"Oranges")
     if dim==2 view2D() end
     plot_points(targetp[:,1:dim],(0.5,0.,0.),scales=0.08,figure=fignum);mlab.colorbar()
 
